246500c47a10b282708226357fee60ec5ada6174
Ahmad Shahidi <ahmadshahidi@gmail.com>
2025-05-26 16:16:42 -0400

    Implement Roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
+        sans: ['Roboto', 'system-ui', 'sans-serif'],
+        lora: ['Lora', 'serif'],
       },
     },
   },

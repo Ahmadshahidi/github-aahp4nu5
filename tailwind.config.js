d32db608b484d243f83bf679c442eff0deca4475
Ahmad Shahidi <ahmadshahidi@gmail.com>
2025-05-26 16:14:54 -0400

    Fix font-heading class definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
-        heading: ['Poppins', 'system-ui', 'sans-serif'],
+        poppins: ['Poppins', 'system-ui', 'sans-serif'],
       },
     },
   },

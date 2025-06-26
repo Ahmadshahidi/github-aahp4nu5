ab3e141d682193b7d41fe13ddf754ccafe4b5954
Ahmad Shahidi <ahmadshahidi@gmail.com>
2025-06-26 11:12:20 -0400

    Updated HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -139,8 +139,6 @@ const HeroSection: React.FC = () => {
       {/* Bottom gradient fade */}
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-white dark:from-gray-900 to-transparent"></div>
     </section>
-  )
-  )
   );
 };
 

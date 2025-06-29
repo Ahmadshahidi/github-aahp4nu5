3b20c416a06f528ab4270cbaf06fc2a7b3a52423
Ahmad Shahidi <ahmadshahidi@gmail.com>
2025-06-29 12:11:49 -0400

    Increase margin between navbar and content in Blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -37,7 +37,7 @@ const Blog: React.FC = () => {
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
       <div className="text-center mb-12">
         <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-4">
           Latest Blog Posts
